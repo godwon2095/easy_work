304e7ca1ae9171336f8b2dfb35a174800587ca3f
mihyeo <2005ch@naver.com>
2022-01-14 09:55:29 +0900

    change

--- live.py
+++ live.py
@@ -51,7 +51,7 @@ try:
         if len(live_time) > 0:
             time_arr.append(live_time)
     
-    for i in range(3, 41):
+    for i in range(3, 49):
         try:
             item = browser.find_elements(By.CSS_SELECTOR, "#scheduleItem > ul:nth-child({}) > li:nth-child(1) > div > strong > a".format(i))
             item_name_arr.append(item[0].text)
